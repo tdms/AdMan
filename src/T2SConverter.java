e3c2705999448f1a7d56a91a4ba870a7f6d07757
sm7hr <sm7hr@Flax.cs.virginia.edu>
2009-09-14 15:18:56 -0400

    Text to Speech Conversion done !

--- src/T2SConverter.java
+++ src/T2SConverter.java
@@ -1,4 +1,56 @@
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.audio.JavaClipAudioPlayer;
+import com.sun.speech.freetts.Utterance;
+import com.sun.speech.freetts.Relation;
+import com.sun.speech.freetts.FreeTTSSpeakable;
+
+
 
 public class T2SConverter {
+	Voice helloVoice;
+	Voice[] voices;
+	
+	T2SConverter()
+	{
+	       
+        /* The VoiceManager manages all the voices for FreeTTS.
+         */
+        VoiceManager voiceManager = VoiceManager.getInstance();
+        System.out.println(voiceManager.toString());
+        
+        /*
+        voices = voiceManager.getVoices();
+        System.out.println("Total Voices: " + voices.length);
+        
+        helloVoice = voices[0];
+        */
+        
+        helloVoice = voiceManager.getVoice("kevin16");
+
+        if (helloVoice == null) {
+            System.err.println("Cannot find this voice. Please specify a different voice.");
+            System.exit(1);
+        }
+        
+        /* Allocates the resources for the voice.
+         */
+        helloVoice.allocate();
+        speak("hello university of virginia.");
+	} 
+	
+	public void speak(String s)
+	{	
+		/* Synthesize speech.
+		*/
+		helloVoice.speak(s);
+	}
+	public void clean()
+	{       
+		helloVoice.deallocate();
+	}
 
 }
+
+
+
